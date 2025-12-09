88146fbc203d141e40a800276f4e33912d9d383d
kollulohithaalekhya <alekhyakollu@gmail.com>
2025-12-09 11:38:48 +0530

    Modified the express example

--- Basics/express_examples.js
+++ Basics/express_examples.js
@@ -2,21 +2,52 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Express Server Running!");
-});
-const users = [];
+let users = [];
 
+// POST: Add user
 app.post("/add-user", (req, res) => {
-  users.push(req.body);
-  res.json({ message: "Data added successfully!", data: req.body });
+  const newUser = req.body;
+  users = users.concat(newUser);
+  res.send("User added successfully");
 });
 
+// GET: All users
 app.get("/users", (req, res) => {
-  res.json(users);
+  res.json({ users });
 });
 
+// GET: Single user by ID
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(u => u.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send(`User with id ${id} not found`);
+  }
+});
+
+// PUT: Update user by ID
+app.put("/update-user/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedData = req.body;
+  let userIndex = users.findIndex(u => u.id === id);
+
+  if (userIndex !== -1) {
+    users[userIndex] = { ...users[userIndex], ...updatedData };
+    res.send(`User with id ${id} updated successfully`);
+  } else {
+    res.status(404).send(`User with id ${id} not found`);
+  }
+});
+
+// DELETE: Remove user by ID
+app.delete("/delete-user/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  users = users.filter(user => user.id !== id);
+  res.send(`User with id ${id} deleted successfully`);
+});
 
 app.listen(5000, () => {
-    console.log("Server started on http://localhost:5000");
+  console.log("Server started on http://localhost:5000");
 });

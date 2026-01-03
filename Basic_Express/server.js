d8f1d1bbe75040a7a40f7b11ef97a116c7d518bd
kollulohithaalekhya <alekhyakollu@gmail.com>
2026-01-03 11:45:21 +0530

    Implement MVC structure in Basic_Express

--- Basic_Express/server.js
+++ Basic_Express/server.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const app=express();
+const FirstRoute=require("./routes/FirstRouter");
+app.use("/",FirstRoute);
 app.listen(7000,()=>{
     console.log('Server running');
 })

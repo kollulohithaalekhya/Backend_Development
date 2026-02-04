0c42295e2bdbd430de3ac71953ef96ff5d8800e2
kollulohithaalekhya <alekhyakollu@gmail.com>
2026-02-04 18:00:43 +0530

    Update App routing and layout

--- tasks/DBFormAndApiTask/frontend/src/App.jsx
+++ tasks/DBFormAndApiTask/frontend/src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from "react";
-import "./App.css";
-import Login from "./LoginForm";
-import Cards from "./Cards";
-import UpdateCard from "./UpdateCards";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddUser from "./AddUser";
+import Users from "./Users";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [cardDetails, setCardDetails] = useState(null);
-
-  const handleLogin = (details) => {
-    setIsLoggedIn(true);
-    setCardDetails(details);
-  };
-
-  const handleUpdate = (updatedDetails) => {
-    setCardDetails(updatedDetails);
-  };
-
   return (
-    <>
-      {!isLoggedIn ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <>
-          <Cards details={cardDetails} />
-          <UpdateCard onUpdate={handleUpdate} />
-        </>
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddUser />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
